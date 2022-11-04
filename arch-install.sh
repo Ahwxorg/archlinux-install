5644e265a128b683521b6643e98ad9e50918c1e2
Ahwx <ahwx@ahwx.org>
2022-11-04 22:22:42 +0100

    change a few things i forgot

--- arch-install.sh
+++ arch-install.sh
@@ -2,6 +2,10 @@
 
 #part1
 echo "Welcome to Ahwx' arch installer script"
+read -p "Continue? [y/n]" continueanswer
+if [[ $continueanswer != y ]] ; then
+  exit
+fi
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys us
@@ -30,7 +34,7 @@ exit
 #part2
 pacman -S --noconfirm sed
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
@@ -43,6 +47,7 @@ echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
 echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
 mkinitcpio -P
+echo "Enter password for root user"
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
 echo "Enter EFI partition: " 
@@ -54,9 +59,9 @@ sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick fzf man-db xwallpaper python-pywal unclutter xclip maim zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse arc-gtk-theme rsync dash xcompmgr libnotify dunst slock jq aria2 cowsay dhcpcd connman wpa_supplicant rsync pamixer mpd ncmpcpp zsh-syntax-highlighting xdg-user-dirs libconfig bluez bluez-utils
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick fzf man-db xwallpaper python-pywal unclutter xclip maim zip unzip p7zip xdotool papirus-icon-theme brightnessctl dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse arc-gtk-theme rsync dash xcompmgr libnotify dunst slock jq aria2 dhcpcd networkmanager wpa_supplicant rsync pamixer mpd ncmpcpp zsh-syntax-highlighting xdg-user-dirs libconfig
 
-systemctl enable connman.service 
+systemctl enable NetworkManager.service 
 
 echo "Enter username:"
 read username
@@ -64,6 +69,7 @@ echo "permit persist :wheel" >> /etc/doas.conf
 echo "permit nopass root" >> /etc/doas.conf
 echo "permit persist $username" >> /etc/doas.conf
 useradd -m -G wheel -s /bin/zsh $username
+echo "Enter password for your user account"
 passwd $username
 echo "Pre-installation finished. Please reboot into the drive now."
 read -p "Do you want to reboot? [y/n]" answer
@@ -71,35 +77,18 @@ if [[ $answer = y ]] ; then
   reboot now
 fi
 
-ai3_path=/home/$username/arch_install3.sh
-sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
-chown $username:$username $ai3_path
-chmod +x $ai3_path
-su -c $ai3_path -s /bin/sh $username
-exit 
-
-#part3
-cd $HOME
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/Ahwxorg/i3.git tmpdotfiles
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-rm -fr tmpdotfiles
-# dwm: Window Manager
 git clone --depth=1 https://github.com/Ahwxorg/dwm.git ~/.local/src/dwm
 sudo make -C ~/.local/src/dwm install
 
-# st: Terminal
 git clone --depth=1 https://github.com/Ahwxorg/pt.git ~/.local/src/st
 sudo make -C ~/.local/src/st install
 
-# dmenu: Program Menu
 git clone --depth=1 https://github.com/Ahwxorg/dmenu.git ~/.local/src/dmenu
 sudo make -C ~/.local/src/dmenu install
 
-# dwmblocks: Status bar for dwm
 git clone --depth=1 https://github.com/Ahwxorg/dwmblocks.git ~/.local/src/dwmblocks
 sudo make -C ~/.local/src/dwmblocks install
 
-# yay: AUR helper
 git clone https://aur.archlinux.org/yay-git.git
 cd yay
 makepkg -si
@@ -110,10 +99,9 @@ mkdir down dox pix dev
 ln -s ~/.config/x11/xinitrc .xinitrc
 ln -s ~/.config/shell/profile .zprofile
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm ~/.zshrc ~/.zsh_history
-wget https://raw.githubusercontent.com/Ahwxorg/i3/main/.zshrc .zshrc
-wget https://raw.githubusercontent.com/Ahwxorg/i3/main/.oh-my-zsh/themes/alanpeabody.zsh-theme ~/.oh-my-zsh/themes/alanpeabody.zsh-theme
-wget https://raw.githubusercontent.com/Ahwxorg/i3/main/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ~/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-wget https://raw.githubusercontent.com/Ahwxorg/i3/main/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-systax-highlighting.zsh ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/Ahwxorg/dotfiles
+cd dotfiles
+mv * ~/
+mv .* ~/
 
 exit
